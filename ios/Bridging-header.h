b36e2ebe0e0b1cbbe57b89ded21c8a7092b952e0
Alexandre Dao <alexandre.dao@epitech.eu>
2021-10-01 01:40:31 +0200

    Detox on ios working

---
+++ ios/Bridging-header.h
@@ -0,0 +1,12 @@
+//
+//  Bridging-header.h
+//  DetoxExample
+//
+//  Created by Alexandre Dao on 01/10/2021.
+//
+
+#ifndef Bridging_header_h
+#define Bridging_header_h
+
+
+#endif /* Bridging_header_h */
---
+++ ios/BridgingHeader.swift
@@ -0,0 +1,8 @@
+//
+//  BridgingHeader.swift
+//  DetoxExample
+//
+//  Created by Alexandre Dao on 01/10/2021.
+//
+
+import Foundation
